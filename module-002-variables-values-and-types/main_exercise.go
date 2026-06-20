f3f06fff8efb671be00eed80b5a89bd865d28d49
Alex H. Raber <alexhraber@gmail.com>
2026-06-19 17:21:42 -0700

    002 exercise completed

---
+++ module-002-variables-values-and-types/main_exercise.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	var learnerName string
+	var learnerCountry string
+	var lessonNumber int
+	var isReady bool
+	var isOn bool
+
+	topic := "values have types"
+	count := 42
+	complete := true
+
+	fmt.Println(learnerName)
+	fmt.Println(learnerCountry)
+	fmt.Println(lessonNumber)
+	fmt.Println(isReady)
+	fmt.Println(isOn)
+
+	fmt.Println(topic)
+	fmt.Println(count)
+	fmt.Println(complete)
+}
